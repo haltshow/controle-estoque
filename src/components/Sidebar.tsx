a07b0a535c014395ae7d764567109e6a7500f043
Gabriel Abreu <gabrielabreualves@gmail.com>
2024-08-13 21:48:43 -0300

    alteração sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { Button } from "@/components/ui/button"
 
 export default function Sidebar() {
     const menu = [{
@@ -25,17 +26,23 @@ export default function Sidebar() {
     ]
 
     return (
-        <header className="min-h-screen w-64 bg-stone-900 p-8 flex flex-col items-center space-y-16 border-r-2 border-cyan-500">
-            <Image src="/logo.png" width={100} height={100} alt="Logo" />
-           <ul className="flex flex-col justify-center space-y-8">
-                {menu && menu.map((m, id) => {
-                    return (
-                        <li key={id}> 
-                            <a href={m.link} className="cursor-pointer"> {m.label} </a>
-                        </li>
-                    )
-                })}
-           </ul>
+        <header className="min-h-screen w-64 bg-stone-900 p-8 flex flex-col items-center justify-between space-y-16 border-r-2 border-cyan-500">
+            <div className="flex flex-col items-center gap-16">
+                <Image src="/logo.png" width={100} height={100} alt="Logo" />
+                <ul className="flex flex-col justify-center space-y-8">
+                        {menu && menu.map((m, id) => {
+                            return (
+                                <li key={id}> 
+                                    <a href={m.link} className="cursor-pointer"> {m.label} </a>
+                                </li>
+                            )
+                        })}
+                </ul>
+            </div>
+            <div className="flex flex-col gap-8">
+                <p><a href="/settings">Configurações</a></p>
+                <Button className="text-base"> Sair </Button> 
+            </div>
         </header>
     )
 }
